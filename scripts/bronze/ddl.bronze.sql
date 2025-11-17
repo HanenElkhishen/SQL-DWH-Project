df298e998eca88c3d76166ab41a5f43528ce8da8
Hanen EL-Khishen <hanenhosam16@gmail.com>
2025-11-17 12:10:33 +0200

    Update ddl.bronze.sql

--- scripts/bronze/ddl.bronze.sql
+++ scripts/bronze/ddl.bronze.sql
@@ -1,95 +1,90 @@
-
 /*
 ===============================================================================
-Stored Procedure: Load Bronze Layer (Source -> Bronze)
+DDL Script: Create Bronze Tables
 ===============================================================================
 Script Purpose:
-    This stored procedure loads data into the 'bronze' schema from external CSV files. 
-    It performs the following actions:
-      - Truncates the bronze tables before loading data.
-      - Uses BULK INSERT to load data from CSV files into bronze tables.
-
-Parameters:
-    none
-Usage Example:
-    EXEC bronze.load_bronze;
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze 
-AS
-BEGIN
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
 
-    /*=====================================================================
-        SECTION 1: Load CRM Data into Bronze
-      =====================================================================*/
-    PRINT '================ Loading CRM Data into Bronze ================';
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
+GO
 
-    ---------------------- CRM: Customer Info ----------------------
-    BULK INSERT bronze.crm_cust_info
-    FROM 'D:\Projects for PORT\SQL Data Warehouse SQL Server-Data Engineering Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        TABLOCK
-    );
-    SELECT * FROM bronze.crm_cust_info;
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
 
-    ---------------------- CRM: Product Info -----------------------
-    BULK INSERT bronze.crm_prd_info
-    FROM 'D:\Projects for PORT\SQL Data Warehouse SQL Server-Data Engineering Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        TABLOCK
-    );
-    SELECT * FROM bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+GO
 
-    ---------------------- CRM: Sales Details ----------------------
-    BULK INSERT bronze.crm_sales_details
-    FROM 'D:\Projects for PORT\SQL Data Warehouse SQL Server-Data Engineering Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        TABLOCK
-    );
-    SELECT * FROM bronze.crm_sales_details;
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
 
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
 
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
 
-    /*=====================================================================
-        SECTION 2: Load ERP Data into Bronze
-      =====================================================================*/
-    PRINT '================ Loading ERP Data into Bronze ================';
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
 
-    ---------------------- ERP: Customer AZ12 ----------------------
-    BULK INSERT bronze.erp_cust_az12
-    FROM 'D:\Projects for PORT\SQL Data Warehouse SQL Server-Data Engineering Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        TABLOCK
-    );
-    SELECT * FROM bronze.erp_cust_az12;
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
 
-    ---------------------- ERP: Location A101 ----------------------
-    BULK INSERT bronze.erp_loc_a101
-    FROM 'D:\Projects for PORT\SQL Data Warehouse SQL Server-Data Engineering Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        TABLOCK
-    );
-    SELECT * FROM bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
 
-    ---------------------- ERP: PX Category G1V2 -------------------
-    BULK INSERT bronze.erp_px_cat_g1v2
-    FROM 'D:\Projects for PORT\SQL Data Warehouse SQL Server-Data Engineering Project\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        TABLOCK
-    );
-    SELECT * FROM bronze.erp_px_cat_g1v2;
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
 
-END;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+GO
